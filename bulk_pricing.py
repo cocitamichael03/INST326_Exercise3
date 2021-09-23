fc8dd6760e9a2ecfca3ed5853cbdc9c49198c4ee
Jorge Argueta <67116860+Jargueta1@users.noreply.github.com>
2021-09-23 15:25:06 -0400

    restarted

--- bulk_pricing.py
+++ bulk_pricing.py
@@ -1,13 +1,1 @@
-"""Exercise 1 for INST 326, creates a simple game of rock, paper and scissors. 
-Driver: Jorge Argueta; jarguet2@umd.edu
-Navigator: Michael Vincent Cocita
-Assignment: Exercise 1 
-Date: 9_9_21
-Challenges Encountered:  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 
-"""
 
-
-
-if __name__ =="__main__":
-
-    pass
