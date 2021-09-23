41ef53d31b769f3d4326ba604b648aa3f408e5e6
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-09-23 15:27:43 -0400

    Update bulk_pricing.py

--- bulk_pricing.py
+++ bulk_pricing.py
@@ -1,1 +1,24 @@
 
+import sys
+from argparse import ArgumentParser
+
+def get_cost(number_of_magnets):
+
+    return(float(0) if number_of_magnets < 0 else
+    float(.75) if 0 <= number_of_magnets < 50 else
+    float(.72) if 99 >= number_of_magnets >= 50 else
+    float(.7) if 999 >= number_of_magnets >= 100 else
+    float(.67))
+  
+
+def parse_args(args_list):
+    
+    parser = ArgumentParser()
+    parser.add_argument("number_of_magnets", type = int)
+    args = parser.parse_args(args_list)
+    return args
+
+
+if __name__ == "__main__":
+    arguments = parse_args(sys.argv[1:])
+    print(get_cost(arguments.number_of_magnets))
