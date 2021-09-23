2cba74a9d51017b896babb8aaec6d9eae0db9368
cocitamichael03 <65200739+cocitamichael03@users.noreply.github.com>
2021-09-23 16:05:59 -0400

    Finished the code

--- _test.py
+++ _test.py
@@ -1,1 +1,15 @@
-## This is a test 
+import math
+from bulk_pricing import get_cost
+
+def test_bulk_pricing_happy_path():
+    assert math.isclose(get_cost(5), 3.75)
+    assert math.isclose(43.2, get_cost(60))
+    assert math.isclose(get_cost(500), 350)
+    assert math.isclose(get_cost(1500), 1005)
+
+def test_bulk_pricing_edge_cases():
+    assert math.isclose(get_cost(49), 36.75)
+    assert math.isclose(get_cost(50), 36)
+    assert math.isclose(get_cost(999), 699.3)
+    assert math.isclose(get_cost(1000), 670)
+
--- bulk_pricing.py
+++ bulk_pricing.py
@@ -8,17 +8,8 @@ def get_cost(number_of_magnets):
     float(.75) if 0 <= number_of_magnets < 50 else
     float(.72) if 99 >= number_of_magnets >= 50 else
     float(.7) if 999 >= number_of_magnets >= 100 else
-    float(.67))
-  
-
-def parse_args(args_list):
-    
-    parser = ArgumentParser()
-    parser.add_argument("number_of_magnets", type = int)
-    args = parser.parse_args(args_list)
-    return args
+    float(.67))*number_of_magnets
 
 
 if __name__ == "__main__":
-    arguments = parse_args(sys.argv[1:])
-    print(get_cost(arguments.number_of_magnets))
+    print(get_cost(1000))
